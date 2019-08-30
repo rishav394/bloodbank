efabb3beded85f840ad719e571cb9fe8cfec0145
Rishav Rungta <rishav394@gmail.com>
2019-08-31 01:04:10 +0530

    Populate fix

--- util/populator.js
+++ util/populator.js
@@ -1,5 +1,4 @@
-require('../index.js');
-const path = require('path');
+const mongoose = require('mongoose');
 var names = require('./names.json');
 var addresses = require('./addresses.json');
 var cities = require('./cities.json');
@@ -8,23 +7,39 @@ var bloods = require('./bloods.json');
 
 const userModel = require('../models/user');
 
-phones = [...new Set(phones)];
-console.log(phones.length);
-
-phones.forEach((phone) => {
-	new userModel({
-		name: names[Math.floor(Math.random() * names.length)],
-		bloodGroup: bloods[Math.floor(Math.random() * bloods.length)],
-		city: cities[Math.floor(Math.random() * cities.length)],
-		phone: phone,
-		amount: Math.floor(Math.random() * 10),
-		address: addresses[Math.floor(Math.random() * addresses.length)],
-	})
-		.save()
-		.then((user) => {
-			console.log(user.phone);
-		})
-		.catch((err) => {
-			// console.log(err.errmsg);
-		});
+mongoose.connection.on('open', () => {
+	populate();
 });
+
+require('../index.js');
+
+const populate = () => {
+	console.log('Ready to populate!');
+	phones = [...new Set(phones)];
+	phones.forEach((phone) => {
+		new userModel({
+			name: names[Math.floor(Math.random() * names.length)],
+			bloodGroup: bloods[Math.floor(Math.random() * bloods.length)],
+			city: cities[Math.floor(Math.random() * cities.length)],
+			phone: phone,
+			amount: Math.floor(Math.random() * 10),
+			address: addresses[Math.floor(Math.random() * addresses.length)],
+		})
+			.save()
+			.then((user) => {
+				console.log(user.phone, ' added!');
+			})
+			.catch((err) => {
+				// console.log(err.errmsg);
+			});
+	});
+	require('fs').writeFile(
+		__dirname + '/phone.json',
+		JSON.stringify(phones, null, 4),
+		'utf8',
+		(err) => {
+			if (err) console.log(err);
+		},
+	);
+	console.log('Finished Populating!');
+};
